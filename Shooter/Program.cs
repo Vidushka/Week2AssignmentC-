a5ec80693d1070d359a7c660d9a02397d4469e41
HsenidVidushka <vidushka@hsenidlanka.com>
2017-04-20 15:52:48 +0530

    Week2Assignment

---
+++ Shooter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter
+{
+    class Program
+    {
+        static void Main()
+        {
+            int strength;
+            int life = 100;
+            int newStrength;
+
+            while (life > 0)
+            {               
+            Console.WriteLine("Give strength....");
+            newStrength = Int32.Parse(Console.ReadLine());
+            if(newStrength <= 100 && newStrength >=50)
+            {
+                strength = newStrength;
+                    for (; strength > 50; strength--)
+                    {
+                        Console.WriteLine("--->");
+                    }
+                    life = life - 5;
+                }
+            else
+            {
+                Console.WriteLine("Strength should less than 100 and higher than 50.");
+            }
+                
+            }
+            Console.WriteLine("You can't shoot anymore.....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week2Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter year...");
+            int year = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Month...");
+            int month = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Day...");
+            int day = Int32.Parse(Console.ReadLine());
+
+            DateTime dt = new DateTime(year, month, day);
+            Console.WriteLine(dt.ToString("F",
+                        CultureInfo.CreateSpecificCulture("en-US")));
+            Console.ReadKey();
+        }
+    }
+}
